4c67b0a955c73afb53711f58d5cc3530352ee5ed
Rifaat-SaroutB09 <rifaat.sarout2@gmail.com>
2022-12-21 07:13:57 +0200

    Weather API-HTML-CSS

--- weather-api/project/src/App.js
+++ weather-api/project/src/App.js
@@ -1,19 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { component } from "react";
+import Search from './components/Search';
+import WeatherItem from './components/WeatherItem'
+import ContentItem from './components/ContentItem'
+import clear from "./img/weather-icons/clear.svg"
+import cloudy from "./img/weather-icons/cloudy.svg"
+import drizzle from "./img/weather-icons/drizzle.svg"
+import fog from "./img/weather-icons/fog.svg"
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg"
+import partlycloudy from "./img/weather-icons/partlycloudy.svg"
+import rain from "./img/weather-icons/rain.svg"
+import snow from "./img/weather-icons/snow.svg"
+import storm from "./img/weather-icons/storm.svg"
+import unknown from "./img/weather-icons/unknown.svg"
+import {Main} from './App1.js';
+const API_KEY="ff47fd20a55ff2b536cbd902b85ebb9e";
 
-function App() 
 
 
-return (
-  <div className="App">
-  <p>
-   NAME: Rifaat ALSAROUT
-  </p>
+class App extends component {
+  (
+
+    <div>
+
+    </div>
+       
+
+  )
+                                                
+                                              
+  }
+
+export default App;
 
 
-
-  </div>
- )
  
     // <div className="App">
     //   <header className="App-header">
